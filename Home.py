754ab1b3a76e1953e56e8d70c53e73c46fa2e103
Noah0071 <664230007@webmail.npru.ac.th>
2025-11-19 15:44:51 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -19,7 +19,7 @@ with col2:
 with col3:
    st.header("Setosa")
    st.image("./img/iris3.jpg")
-   
+
 html_7 = """
 <div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>สถิติข้อมูลดอกไม้</h5></center>
@@ -27,3 +27,14 @@ html_7 = """
 """
 st.markdown(html_7, unsafe_allow_html=True)
 st.markdown("")
+
+dt = pd.read_csv("./data/iris.csv")
+st.write(dt.head(10))
+
+dt1 = dt['petal.length'].sum()
+dt2 = dt['petal.width'].sum()
+dt3 = dt['sepal.length'].sum()
+dt4 = dt['sepal.width'].sum()
+
+dx = [dt1, dt2, dt3, dt4]
+dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
